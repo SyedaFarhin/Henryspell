291755c42b689ab542035a6eca3e8e630c20f1d6
Syeda Farhin <syedafarhin18@gmail.com>
2026-01-16 17:09:43 +0530

    env changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
-const app = require('./app');
-const { connectDB } = require('./config');
+
+const app = require('./src/app');
+const { connectDB } = require('./src/config');
+
 const PORT = process.env.PORT || 4000;
 
 connectDB().then(() => {
